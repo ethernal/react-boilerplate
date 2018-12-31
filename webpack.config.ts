12b0f5b1d2ab5c8419476251684181c7d4c79d21
Sebastian Pieczynski <testament777@gmail.com>
2018-12-31 17:23:07 +0100

    Done split webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,77 +1,11 @@
-const path = require("path");
-const webpack            = require("webpack");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin  = require("copy-webpack-plugin");
-
-const HTMLMinifier = require("html-minifier");
-
-//based on: https://blog.wax-o.com/2018/05/webpack-loaders-babel-sourcemaps-react-hot-module-reload-typescript-modules-code-splitting-and-lazy-loading-full-tutorial-to-transpile-and-bundle-your-code/
+const path = require('path');
+const productionConfig = require('./webpackConfig/production');
+const developmentConfig = require('./webpackConfig/development');
 
 module.exports = env => {
-    //eslint-disable-next-line no-console
-    console.log("NODE_ENV:", env.NODE_ENV);
-    const isDevelopment = env.NODE_ENV === "development";
-    const plugins       = [new CleanWebpackPlugin(["dist"])];
+  if (env.NODE_ENV === 'production')
+    return productionConfig(env, path.resolve(__dirname));
 
-    if (isDevelopment) {
-        plugins.push(new webpack.NamedModulesPlugin());
-        plugins.push(new webpack.HotModuleReplacementPlugin());
-    } else {
-        plugins.push(
-            new CopyWebpackPlugin(
-                [
-                    {
-                        from: "src/index.html",
-                        to  : path.resolve(__dirname, "dist"),
-                        transform(htmlAsBuffer) {
-                            return Buffer.from(
-                                HTMLMinifier.minify(
-                                    htmlAsBuffer.toString("utf8"),
-                                    {
-                                        collapseWhitespace         : true,
-                                        collapseBooleanAttributes  : true,
-                                        collapseInlineTagWhitespace: true
-                                    }
-                                )
-                            );
-                        }
-                    }
-                ],
-                {}
-            )
-        );
-    }
-
-    let module = {};
-    if (!isDevelopment)
-        module = {
-            rules: [
-                {
-                    test   : /\.js$/,
-                    exclude: /(node_modules)/,
-                    use    : {
-                        loader: "babel-loader"
-                    }
-                }
-            ]
-        };
-
-    return {
-        entry : "./src/index.js",
-        output: {
-            path      : path.resolve(__dirname, "dist/"),
-            filename  : "index.js",
-            publicPath: "/dist/"
-        },
-        mode     : isDevelopment ? "development": "production",
-        devServer: {
-            contentBase     : path.resolve(__dirname, "src"),
-            hotOnly         : true,
-            overlay         : true,
-            publicPath      : "/dist/",
-            watchContentBase: false
-        },
-        module,
-        plugins
-    };
+  if (env.NODE_ENV === 'development')
+    return developmentConfig(env, path.resolve(__dirname));
 };
