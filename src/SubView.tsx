7ed092492402ce1e82c3fff8d2b80c9e9fd36898
Sebastian Pieczynski <testament777@gmail.com>
2019-01-01 12:49:02 +0100

    Commit deleted files

--- src/SubView.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export interface PropsInterface {
-    counter: number;
-}
-const SubView: React.SFC<PropsInterface> = ({ counter }) => (
-    <div>This is a subview created when the counter was {counter}</div>
-);
-export default SubView;
--- src/index.js
+++
@@ -1,4 +0,0 @@
-if (module.hot) {
-    module.hot.accept();
-}
-console.log("Hi");
